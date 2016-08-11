9f094da70ba469f876f057c7dc7c821ba6d2b47b
MishaVolinets <Volinetsmisha@gmail.com>
2016-08-12 00:53:30 +0300

    MyCommit

---
+++ com/MishaVolinets/Main.java
@@ -0,0 +1,12 @@
+package com.MishaVolinets;
+
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args){
+		int someNumber = 10;
+		int increasedNumber = ++someNumber;
+		System.out.println(increasedNumber);
+	}
+}
